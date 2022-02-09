4d34c2ad59640bfae4193d26b577587fd4d6c2de
Sy Tung An <sytungan@gmail.com>
2022-02-09 20:18:46 +0700

    React and reply message basic

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+const { HEXTECH_TOKEN } = require("./environments");
+
+const { Client, Intents } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === "ping") {
+        await interaction.reply("Pong!");
+    }
+});
+
+client.on("messageCreate", function (message) {
+    if (message.content.includes('tri')) message.react('✅')
+    if (message.content.includes('bot')) message.reply('hello')
+    if (message.content.includes('ngu qua')) message.channel.send("may ngu vl")
+});
+
+client.login(HEXTECH_TOKEN);
---
+++ environments/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const CLIENT_ID = process.env.CLIENT_ID
+const GUILD_ID = process.env.GUILD_ID
+const HEXTECH_TOKEN = process.env.HEXTECH_TOKEN
+
+module.exports = {
+    CLIENT_ID,
+    GUILD_ID,
+    HEXTECH_TOKEN
+}
